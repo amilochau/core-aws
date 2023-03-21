ab5f65e1806dbfcd561fa02c789ed1bdd704eb57
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-03-21 15:15:36 +0100

    Support anonymous requests for integration (#7)

--- src/Libraries Projects/Milochau.Core.Aws.Integration/ProxyRequestOptions.cs
+++ src/Libraries Projects/Milochau.Core.Aws.Integration/ProxyRequestOptions.cs
@@ -5,13 +5,19 @@ namespace Milochau.Core.Aws.Integration
     /// <summary>Proxy request options</summary>
     public class ProxyRequestOptions
     {
+        /// <summary>Whether the request is anonymous</summary>
+        public bool AnonymousRequest { get; set; }
+
         /// <summary>User id</summary>
+        /// <remarks>Is not used if <see cref="AnonymousRequest"/> is set to true</remarks>
         public string UserId { get; set; } = "00000000000000000000000000000001";
 
         /// <summary>User name</summary>
+        /// <remarks>Is not used if <see cref="AnonymousRequest"/> is set to true</remarks>
         public string UserName { get; set; } = "John Doe";
 
         /// <summary>User emails</summary>
+        /// <remarks>Is not used if <see cref="AnonymousRequest"/> is set to true</remarks>
         public string UserEmail { get; set; } = "john.doe@milochau.com";
 
         /// <summary>Path parameters</summary>
