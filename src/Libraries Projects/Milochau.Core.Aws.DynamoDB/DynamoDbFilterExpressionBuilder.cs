1b8edd2040a497df97241f6f5ae57d47737be71a
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2023-04-04 18:09:16 +0200

    Add builders for DynamoDB expressions and conditions

---
+++ src/Libraries Projects/Milochau.Core.Aws.DynamoDB/DynamoDbFilterExpressionBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milochau.Core.Aws.DynamoDB
+{
+    /// <summary>Filter expression builder for DynamoDB</summary>
+    public class DynamoDbFilterExpressionBuilder
+    {
+        /// <summary>Equal filters</summary>
+        public ICollection<string> Equal { get; set; } = new HashSet<string>();
+
+        /// <summary>Contains filters</summary>
+        public ICollection<string> Contains { get; set; } = new HashSet<string>();
+
+        /// <summary>AttributeNotExists filters</summary>
+        public ICollection<string> AttributeNotExists { get; set; } = new HashSet<string>();
+
+        /// <summary>Get the expression attribute names as a dictionary</summary>
+        public Dictionary<string, string> GetExpressionAttributeNames()
+        {
+            var expressionAttributeNames = new List<KeyValuePair<string, string>>();
+            expressionAttributeNames.AddExpressionAttributeNames(Equal.ToArray());
+            expressionAttributeNames.AddExpressionAttributeNames(Contains.ToArray());
+            expressionAttributeNames.AddExpressionAttributeNames(AttributeNotExists.ToArray());
+            return expressionAttributeNames.ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        /// <summary>Build the filter expression</summary>
+        public string Build()
+        {
+            var items = new List<string>();
+            items.AddRange(Equal.Select(x => $"#{x} = :v_{x}"));
+            items.AddRange(Contains.Select(x => $"contains(#{x}, :v_{x})"));
+            items.AddRange(AttributeNotExists.Select(x => $"attribute_not_exists(#{x})"));
+            return new StringBuilder().AppendJoin(" AND ", items).ToString();
+        }
+    }
+}
---
+++ src/Libraries Projects/Milochau.Core.Aws.DynamoDB/DynamoDbKeyConditionExpressionBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milochau.Core.Aws.DynamoDB
+{
+    /// <summary>Key condition expression builder for DynamoDB</summary>
+    public class DynamoDbKeyConditionExpressionBuilder
+    {
+        /// <summary>Equal condition expressions</summary>
+        public ICollection<string> Equal { get; set; } = new HashSet<string>();
+
+        /// <summary>Smaller condition expressions</summary>
+        public ICollection<string> Smaller { get; set; } = new HashSet<string>();
+
+        /// <summary>Greater condition expressions</summary>
+        public ICollection<string> Greater { get; set; } = new HashSet<string>();
+
+        /// <summary>Get the expression attribute names as a dictionary</summary>
+        public Dictionary<string, string> GetExpressionAttributeNames()
+        {
+            var expressionAttributeNames = new List<KeyValuePair<string, string>>();
+            expressionAttributeNames.AddExpressionAttributeNames(Equal.ToArray());
+            expressionAttributeNames.AddExpressionAttributeNames(Smaller.ToArray());
+            expressionAttributeNames.AddExpressionAttributeNames(Greater.ToArray());
+            return expressionAttributeNames.ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        /// <summary>Build the key condition expression</summary>
+        public string Build()
+        {
+            var items = new List<string>();
+            items.AddRange(Equal.Select(x => $"#{x} = :v_{x}"));
+            items.AddRange(Smaller.Select(x => $"#{x} < :v_{x}"));
+            items.AddRange(Greater.Select(x => $"#{x} > :v_{x}"));
+            return new StringBuilder().AppendJoin(" AND ", items).ToString();
+        }
+    }
+}
