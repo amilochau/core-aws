c54f1e6932064769078660d50fe17d9555883638
antoine-milochau-shift <48644548+amilochau@users.noreply.github.com>
2023-04-07 17:15:14 +0200

    Add more expressions for DynamoDB filters and key conditions

--- src/Libraries Projects/Milochau.Core.Aws.DynamoDB/DynamoDbKeyConditionExpressionBuilder.cs
+++ src/Libraries Projects/Milochau.Core.Aws.DynamoDB/DynamoDbKeyConditionExpressionBuilder.cs
@@ -1,30 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Milochau.Core.Aws.DynamoDB
 {
     /// <summary>Key condition expression builder for DynamoDB</summary>
     public class DynamoDbKeyConditionExpressionBuilder
     {
-        /// <summary>Equal condition expressions</summary>
+        /// <summary>'Equal to' condition expressions</summary>
         public ICollection<string> Equal { get; set; } = new HashSet<string>();
 
-        /// <summary>Smaller condition expressions</summary>
-        public ICollection<string> Smaller { get; set; } = new HashSet<string>();
+        /// <summary>'Less than' condition expressions</summary>
+        public ICollection<string> Less { get; set; } = new HashSet<string>();
 
-        /// <summary>Greater condition expressions</summary>
+        /// <summary>'Less than or equal to' condition expressions</summary>
+        public ICollection<string> LessOrEqual { get; set; } = new HashSet<string>();
+
+        /// <summary>'Greater than' condition expressions</summary>
         public ICollection<string> Greater { get; set; } = new HashSet<string>();
+
+        /// <summary>'Greater than or equal to' condition expressions</summary>
+        public ICollection<string> GreaterOrEqual { get; set; } = new HashSet<string>();
+
+        /// <summary>'Between' condition expressions</summary>
+        public ICollection<string> Between { get; set; } = new HashSet<string>();
+
+        /// <summary>'Begins with' condition expressions</summary>
+        public ICollection<string> BeginsWith{ get; set; } = new HashSet<string>();
 
         /// <summary>Get the expression attribute names as a dictionary</summary>
         public Dictionary<string, string> GetExpressionAttributeNames()
         {
-            var expressionAttributeNames = new List<KeyValuePair<string, string>>();
+            var expressionAttributeNames = new HashSet<KeyValuePair<string, string>>(new KeyValuePairEqualityComparer());
             expressionAttributeNames.AddExpressionAttributeNames(Equal.ToArray());
-            expressionAttributeNames.AddExpressionAttributeNames(Smaller.ToArray());
+            expressionAttributeNames.AddExpressionAttributeNames(Less.ToArray());
+            expressionAttributeNames.AddExpressionAttributeNames(LessOrEqual.ToArray());
             expressionAttributeNames.AddExpressionAttributeNames(Greater.ToArray());
+            expressionAttributeNames.AddExpressionAttributeNames(GreaterOrEqual.ToArray());
+            expressionAttributeNames.AddExpressionAttributeNames(Between.ToArray());
+            expressionAttributeNames.AddExpressionAttributeNames(BeginsWith.ToArray());
             return expressionAttributeNames.ToDictionary(x => x.Key, x => x.Value);
         }
 
@@ -33,9 +49,29 @@ namespace Milochau.Core.Aws.DynamoDB
         {
             var items = new List<string>();
             items.AddRange(Equal.Select(x => $"#{x} = :v_{x}"));
-            items.AddRange(Smaller.Select(x => $"#{x} < :v_{x}"));
+            items.AddRange(Less.Select(x => $"#{x} < :v_{x}"));
+            items.AddRange(LessOrEqual.Select(x => $"#{x} <= :v_{x}"));
             items.AddRange(Greater.Select(x => $"#{x} > :v_{x}"));
+            items.AddRange(GreaterOrEqual.Select(x => $"#{x} >= :v_{x}"));
+            items.AddRange(Between.Select(x => $"#{x} BETWEEN :v_{x}_1 AND :v_{x}_2"));
+            items.AddRange(BeginsWith.Select(x => $"begins_with(#{x}, :v_{x})"));
             return new StringBuilder().AppendJoin(" AND ", items).ToString();
+        }
+
+        /// <summary>Equality comparer to compare key value pairs, based on keys</summary>
+        private class KeyValuePairEqualityComparer : IEqualityComparer<KeyValuePair<string, string>>
+        {
+            /// <summary>Whether the two items are equal</summary>
+            public bool Equals(KeyValuePair<string, string> x, KeyValuePair<string, string> y)
+            {
+                return x.Key.Equals(y.Key, StringComparison.OrdinalIgnoreCase);
+            }
+
+            /// <summary>The hash code of an item</summary>
+            public int GetHashCode([DisallowNull] KeyValuePair<string, string> obj)
+            {
+                return obj.GetHashCode();
+            }
         }
     }
 }
