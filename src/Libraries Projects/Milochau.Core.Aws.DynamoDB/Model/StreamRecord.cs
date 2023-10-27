e0675987d5f2c29ce8580db4352df0f09d445f43
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2023-10-27 10:35:25 +0200

    Fix ApproximateCreationDateTime from StreamRecord

--- src/Libraries Projects/Milochau.Core.Aws.DynamoDB/Model/StreamRecord.cs
+++ src/Libraries Projects/Milochau.Core.Aws.DynamoDB/Model/StreamRecord.cs
@@ -16,7 +16,7 @@ namespace Milochau.Core.Aws.DynamoDB.Model
         /// epoch time</a> format and rounded down to the closest second.
         /// </para>
         /// </summary>
-        public DateTime ApproximateCreationDateTime { get; set; }
+        public long ApproximateCreationDateTime { get; set; }
 
         /// <summary>
         /// Gets and sets the property Keys. 
