5d2e2001593545802cdadc08a37904b4d2ff5c86
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2024-02-23 08:25:19 +0100

    Add support for GUID-keyed dictionaries

--- src/Libraries/Milochau.Core.Aws.Core/JsonConverters/GuidConverter.cs
+++ src/Libraries/Milochau.Core.Aws.Core/JsonConverters/GuidConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text.Json.Serialization;
 using System.Text.Json;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Milochau.Core.Aws.Core.JsonConverters
 {
@@ -15,6 +16,17 @@ namespace Milochau.Core.Aws.Core.JsonConverters
         public override void Write(Utf8JsonWriter writer, Guid value, JsonSerializerOptions options)
         {
             writer.WriteStringValue(value.ToString("N"));
+        }
+
+        public override Guid ReadAsPropertyName(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            return value == null ? default : Guid.Parse(value);
+        }
+
+        public override void WriteAsPropertyName(Utf8JsonWriter writer, [DisallowNull] Guid value, JsonSerializerOptions options)
+        {
+            writer.WritePropertyName(value.ToString("N"));
         }
     }
 }
