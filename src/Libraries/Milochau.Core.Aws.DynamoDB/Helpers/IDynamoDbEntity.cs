90e0ec927ac7d46fa83b1409e26f486912ab5f86
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2024-02-29 15:22:06 +0100

    Minor interface improvement

--- src/Libraries/Milochau.Core.Aws.DynamoDB/Helpers/IDynamoDbEntity.cs
+++ src/Libraries/Milochau.Core.Aws.DynamoDB/Helpers/IDynamoDbEntity.cs
@@ -10,11 +10,19 @@ namespace Milochau.Core.Aws.DynamoDB.Helpers
     /// <summary>
     /// Interface for DynamoDB entity
     /// </summary>
-    public interface IDynamoDbEntity<TEntity> where TEntity: IDynamoDbEntity<TEntity>
+    public interface IDynamoDbEntity<TEntity> : IDynamoDbParsableEntity<TEntity>
+        where TEntity: IDynamoDbEntity<TEntity>
     {
         /// <summary>Format entity for DynamoDB</summary>
         Dictionary<string, AttributeValue> FormatForDynamoDb();
+    }
 
+    /// <summary>
+    /// Interface for DynamoDB parsable entity
+    /// </summary>
+    public interface IDynamoDbParsableEntity<TEntity>
+        where TEntity : IDynamoDbParsableEntity<TEntity>
+    {
         /// <summary>Parse from DynamoDB to entity</summary>
         abstract static TEntity ParseFromDynamoDb(Dictionary<string, AttributeValue> attributes);
     }
