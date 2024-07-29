363f96b08f110460075a6e1184f328fa8765dce4
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2024-07-29 12:29:46 +0200

    Send XRay segments asynchronously

--- src/Libraries/Milochau.Core.Aws.Core/XRayRecorder/Core/Internal/Emitters/UdpSegmentEmitter.cs
+++ src/Libraries/Milochau.Core.Aws.Core/XRayRecorder/Core/Internal/Emitters/UdpSegmentEmitter.cs
@@ -34,8 +34,9 @@ namespace Milochau.Core.Aws.Core.XRayRecorder.Core.Internal.Emitters
             {
                 var packet = segment.Marshall()!;
                 var data = Encoding.ASCII.GetBytes(packet);
-                var ip = daemonConfig.UDPEndpoint; //Need local var to ensure ip do not 
+                var ip = daemonConfig.UDPEndpoint; //Need local var to ensure ip do not
                 udpClient.Send(data, data.Length, ip);
+                udpClient.BeginSend(data, data.Length, ip, null, null);
             }
             catch (SocketException)
             {
