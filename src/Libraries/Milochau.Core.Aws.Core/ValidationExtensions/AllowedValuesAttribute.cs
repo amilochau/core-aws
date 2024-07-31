7ed8a4491832a359f8eae74f0663bf078fa0587a
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2024-07-31 22:48:18 +0200

    Add validation extensions methods for allowed values

---
+++ src/Libraries/Milochau.Core.Aws.Core/ValidationExtensions/AllowedValuesAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Milochau.Core.Aws.Core.ValidationExtensions
+{
+    /// <summary>
+    /// Validation attribute to assert a property, field or parameter is part of a list of allowed values
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class AllowedValuesAttribute(params object[] allowedValues) : ValidationAttribute
+    {
+        public object[] AllowedValues { get; } = allowedValues;
+
+        public override bool IsValid(object? value)
+        {
+            // Automatically pass if value is null. RequiredAttribute should be used to assert a value is not null.
+            if (value == null)
+            {
+                return true;
+            }
+
+            return AllowedValues.Contains(value);
+        }
+    }
+}
