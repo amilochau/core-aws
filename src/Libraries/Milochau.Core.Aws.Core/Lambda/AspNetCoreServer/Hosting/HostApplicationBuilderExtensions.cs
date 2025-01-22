e742729829b1e09c9d30644dfbb628a85c9c470f
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2025-01-22 13:11:25 +0100

    Add `JsonOptions` in integration host builder settings (#90)

--- src/Libraries/Milochau.Core.Aws.Core/Lambda/AspNetCoreServer/Hosting/HostApplicationBuilderExtensions.cs
+++ src/Libraries/Milochau.Core.Aws.Core/Lambda/AspNetCoreServer/Hosting/HostApplicationBuilderExtensions.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Authorization;
 using System;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Milochau.Core.Aws.Abstractions;
+using System.Linq;
+using System.Security.Claims;
 
 namespace Microsoft.AspNetCore.Builder
 {
@@ -26,6 +30,22 @@ namespace Microsoft.AspNetCore.Builder
             builder.Services.AddAuthenticationCore();
             builder.Services.ConfigureHttpJsonOptions(options.JsonOptions);
             builder.Services.AddAuthorizationBuilder().SetFallbackPolicy(new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build());
+
+            // HTTP Context and identity
+            builder.Services.AddHttpContextAccessor();
+            builder.Services.AddScoped(serviceProvider =>
+            {
+                IHttpContextAccessor httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+                ArgumentNullException.ThrowIfNull(httpContextAccessor.HttpContext);
+                var claims = httpContextAccessor.HttpContext.User.Claims;
+                return new IdentityUser
+                (
+                    sub: claims.First(x => x.Type == ClaimTypes.NameIdentifier || x.Type == "sub").Value,
+                    name: claims.First(x => x.Type == "name").Value,
+                    email: claims.First(x => x.Type == ClaimTypes.Email || x.Type == "email").Value,
+                    userId: Guid.Parse(claims.First(x => x.Type == "custom:user_id").Value)
+                );
+            });
 
             builder.Logging.AddLambdaLogger();
 
